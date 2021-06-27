473afc8dd295b6e3a3d2757a793966df1317bdef
Ansi-gif <72799723+Ansi-gif@users.noreply.github.com>
2021-06-27 18:10:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//creating boy running
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+function draw() {
+  background(0);
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  
+  // boy moving on Xaxis with mouse'
+  
+  
+   drawSprites();
+  
+  
+
+}
+
